1ace565ba218cd7d72115fb34c8e4d13c8f1876d
xin7c <xin7c@qq.com>
2017-02-14 18:24:32 +0800

    + static settings

--- acfun_qa_home/settings.py
+++ acfun_qa_home/settings.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding=utf-8
 """
 Django settings for acfun_qa_home project.
 
@@ -121,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# 当运行 python manage.py collectstatic 的时候
+# STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
+# 把这些文件放到一起是为了用apache等部署的时候更方便
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_all')
+
+# 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
+# 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
