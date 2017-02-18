a894e7351c85f3aae76151f91ee43949fd906682
xin7c <xin7c@qq.com>
2017-02-18 17:36:23 +0800

    & testform 优化form样式

--- common_forms/forms.py
+++ common_forms/forms.py
@@ -2,8 +2,10 @@
 from django import forms
 
 class AddForm(forms.Form):
-    a = forms.IntegerField(max_value=5, label="inputA")
-    b = forms.IntegerField(max_value=5, label="inputB")
+    a = forms.IntegerField(max_value=5, label="inputA",
+                           widget=forms.NumberInput(attrs={'placeholder': "<+=5"}))
+    b = forms.IntegerField(max_value=5, label="inputB",
+                           widget=forms.NumberInput(attrs={'placeholder': "<+=5"}))
     c = forms.EmailField(label="inputMail",
                          widget=forms.EmailInput(attrs={'placeholder': "请输入合法Email"}))
 
