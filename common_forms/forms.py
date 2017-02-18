ff2959e76abf73118a9c96a4cbe2d3592441e06d
xin7c <xin7c@qq.com>
2017-02-18 18:11:24 +0800

    & testform 添加autofocus

--- common_forms/forms.py
+++ common_forms/forms.py
@@ -3,11 +3,11 @@ from django import forms
 
 class AddForm(forms.Form):
     a = forms.IntegerField(max_value=5, label="inputA",
-                           widget=forms.NumberInput(attrs={'placeholder': "<+=5"}))
+                           widget=forms.NumberInput(attrs={"placeholder": "<+=5", "autofocus": "autofocus"}))
     b = forms.IntegerField(max_value=5, label="inputB",
-                           widget=forms.NumberInput(attrs={'placeholder': "<+=5"}))
+                           widget=forms.NumberInput(attrs={"placeholder": "<+=5"}))
     c = forms.EmailField(label="inputMail",
-                         widget=forms.EmailInput(attrs={'placeholder': "请输入合法Email"}))
+                         widget=forms.EmailInput(attrs={"placeholder": "请输入合法Email"}))
 
     # d = forms.EmailField(label="inputMail",
     #                      widget=forms.EmailInput(attrs={'class': "form-control no-radius", 'placeholder': u'详细描述', 'rows': 3}))
