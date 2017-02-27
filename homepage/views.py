d3e10db075e6fba3e893bd372b6df9ca175692d8
xin7c <xin7c@qq.com>
2017-02-27 18:49:17 +0800

    push origin

--- homepage/views.py
+++ homepage/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import render
 from homepage.models import Homepagedb, Userdb
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect
 from django import forms
 
 
